ffca6da44fac541beada8f829613583beaf5a8ab
Oswen2002 <oswenisaac@gmail.com>
2025-02-06 16:27:51 -0600

    Navegador funcional con la parte de sobreNosotros

---
+++ src/js/datos/data.js
@@ -0,0 +1,9 @@
+const SobreNosotros = [
+    { pregunta: "¿Cuál es la historia de la UNICAH?", id: "historia" },
+    { pregunta: "¿Qué misión tiene la UNICAH?", id: "mision" },
+    { pregunta: "¿Qué valores promueve la UNICAH?", id: "valores" },
+    { pregunta: "¿Qué visión tiene la UNICAH?", id: "vision" },
+    { pregunta: "¿Cuál es el propósito de la UNICAH?", id: "proposito" },
+    { pregunta: "¿Qué política de calidad tiene UNICAH?", id: "politica-calidad" },
+    { pregunta: "¿Dónde se pueden consultar los estatutos de la UNICAH y qué regulan?", id: "estatutos" }
+];
